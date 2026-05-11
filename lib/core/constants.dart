5cde55d780657d71fb4325563cc77c7ba5bdb227
Alper <alperberke2323@gmail.com>
2026-05-11 13:09:39 +0300

    bildirim_bitti

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   // Backend sunucumuzun ana adresi
-  static const String baseUrl = 'https://smart-trash-bin-nine.vercel.app/api';
+  static const String baseUrl = 'https://smart-trash-bin.onrender.com';
 }
